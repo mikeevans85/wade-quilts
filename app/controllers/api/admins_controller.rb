b59be6449c6d047c10ed6293fbe40a0099b29bfe
mikeevans85 <baudecordier@gmail.com>
2019-03-16 16:54:55 -0500

    Added admin creation

--- app/controllers/api/admins_controller.rb
+++ app/controllers/api/admins_controller.rb
@@ -1,2 +1,18 @@
 class Api::AdminsController < ApplicationController
+
+  def create
+    admin = Admin.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+
+    if admin.save
+      render json: {message: 'User created successfully'}, status: :created
+    else
+      render json: {errors: admin.errors.full_messages}, status: :bad_request
+    end
+  end
 end
